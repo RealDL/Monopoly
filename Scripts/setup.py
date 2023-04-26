6c12725099ec07891b5c07bfde09d16ac0c98fb4
DL <91336924+TheRealDL1@users.noreply.github.com>
2023-04-26 18:05:53 +0100

    Delete Scripts directory

--- Scripts/setup.py
+++
@@ -1,7 +0,0 @@
-
-class Setup(object):
-    def __init__(self):
-        self.startScreen = True
-        self.setUp = False
-        self.numberOfPlayerInGame = False
-        self.MainGame = False
