f33f3ac80ff0bb079c059f67b112ee0ffbe53ee1
DL <91336924+TheRealDL1@users.noreply.github.com>
2023-04-26 18:06:19 +0100

    Add files via upload

---
+++ Scripts/player.py
@@ -0,0 +1,20 @@
+
+class Player(object):
+    def __init__(self, username, token, money=1500):
+        self.money = money
+        self.inJail = False
+        self.playerTurn = False
+        self.houses = 0
+        self.hotels = 0
+        self.properties = 0
+        self.sets = 0
+        self.username = username
+        self.token = token
+        ## self.click = False
+        ## self.hover = False
+    def BuyProperty(self, propertyCost, propertyName, propertyColour, propertyBoardArea):
+        if self.money > propertyCost:
+            self.money -= propertyCost
+            self.properties += 1
+    def draw(self, win, positionX, positionY):
+        win.blit(self.token, (positionX,positionY))
